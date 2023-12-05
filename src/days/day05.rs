0b5b25a36de4e336c115a5e5cd58a6953c034ce4
samooyo <samidarnaud@gmail.com>
2023-12-05 13:35:43 +0100

    day05 part2

--- src/days/day05.rs
+++ src/days/day05.rs
@@ -7,11 +7,21 @@ pub fn run() {
     let (seeds, maps) = binding.split_once("\n\n").unwrap();
     let seeds = seeds.replace("seeds:", "");
 
-    let clean_seeds: Vec<usize> = seeds
+    let seeds_part1: Vec<usize> = seeds
         .replace("seeds:", "")
         .split_whitespace()
         .map(|x| x.parse::<usize>().unwrap())
         .collect();
+
+    let mut seeds_part2 = vec![];
+    for (i, seed) in seeds_part1.iter().enumerate() {
+        if i % 2 == 0 {
+            for j in 0..seeds_part1[i + 1] {
+                seeds_part2.push(seed + j);
+            }
+        }
+    }
+
     let categories: Vec<&str> = maps.split("\n\n").collect();
 
     for category in categories {
@@ -30,7 +40,7 @@ pub fn run() {
     }
 
     let mut res: Vec<usize> = vec![];
-    for seed in clean_seeds {
+    for seed in seeds_part1 {
         let mut location = seed;
         for category in &almanac {
             let mut tmp_seed = location;
@@ -45,4 +55,21 @@ pub fn run() {
         res.push(location);
     }
     println!("part1 : {}", res.iter().min().unwrap());
+
+    let mut res: Vec<usize> = vec![];
+    for seed in seeds_part2 {
+        let mut location = seed;
+        for category in &almanac {
+            let mut tmp_seed = location;
+            for &[destination, source, range] in category {
+                if (source..(source + range)).contains(&tmp_seed) {
+                    tmp_seed = tmp_seed - source + destination;
+                    break;
+                }
+            }
+            location = tmp_seed;
+        }
+        res.push(location);
+    }
+    println!("part2 : {}", res.iter().min().unwrap());
 }
