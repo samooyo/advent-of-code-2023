c8679f4ed0b3e0108a662f4e7d660a3e02567e8d
samooyo <samidarnaud@gmail.com>
2023-12-14 16:29:08 +0100

    day09 part02

--- src/days/day09.rs
+++ src/days/day09.rs
@@ -1,6 +1,7 @@
 pub fn run() {
     let input = include_str!("../../inputs/day09.txt");
-    let mut res = 0;
+    let (mut res, mut res_part2) = (0, 0);
+
     for line in input.lines() {
         let nums = line
             .split_whitespace()
@@ -9,21 +10,34 @@ pub fn run() {
             .map(|x| x.parse::<i32>().unwrap());
 
         let mut derivate: Vec<i32> = nums.clone().collect();
-        let mut derivates: Vec<Vec<i32>> = vec![derivate.clone()];
 
-        while derivate.iter().any(|x| *x != 0) {
-            derivate = derivatee(&derivate);
-            derivates.push(derivate.clone());
+        for part in 1..=2 {
+            if part == 2 {
+                derivate = nums.clone().collect();
+                derivate.reverse();
+            }
+
+            let mut derivates: Vec<Vec<i32>> = vec![derivate.clone()];
+
+            while derivate.iter().any(|x| *x != 0) {
+                derivate = sub(&derivate);
+                derivates.push(derivate.clone());
+            }
+
+            derivates.iter().rev().for_each(|x| {
+                if part == 1 {
+                    res += x.last().unwrap();
+                } else {
+                    res_part2 += x.last().unwrap();
+                }
+            });
         }
-        derivates.iter().rev().for_each(|x| {
-            res += x.last().unwrap();
-        });
     }
 
-    println!("Result : {}", res);
+    println!("Part 1 : {}, part 2 : {}", res, res_part2);
 }
 
-fn derivatee(nums: &Vec<i32>) -> Vec<i32> {
+fn sub(nums: &Vec<i32>) -> Vec<i32> {
     let mut new_nums = nums.clone();
     for i in 1..nums.len() {
         new_nums[i] -= nums[i - 1];
