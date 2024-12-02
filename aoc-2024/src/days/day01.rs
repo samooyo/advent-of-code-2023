36088952935e228dd1f5abe20e0e338c3ba66ab0
samooyo <samidarnaud@gmail.com>
2024-12-02 13:55:17 +0100

    perf(day01): use itertools

--- aoc-2024/src/days/day01.rs
+++ aoc-2024/src/days/day01.rs
@@ -1,15 +1,21 @@
 use anyhow::{anyhow, Context, Result};
+use itertools::Itertools;
 use std::fs;
 
 pub fn run() -> Result<()> {
+    part1()
+}
+
+fn part1() -> Result<()> {
     let input = fs::read_to_string("inputs/day01.txt").context("Reading file")?;
     let mut left_locations: Vec<usize> = Vec::new();
     let mut right_locations: Vec<usize> = Vec::new();
 
     for line in input.lines() {
         let (left, right) = line
-            .split_once("   ")
-            .ok_or(anyhow!("Couldn't find separator"))
+            .split_whitespace()
+            .collect_tuple()
+            .ok_or_else(|| anyhow!("Couldn't find separator"))
             .context("Parsing line")?;
 
         left_locations.push(left.parse()?);
