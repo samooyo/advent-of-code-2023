0c93248ab2df793798b01532cc17e4e0c579b59d
samooyo <samidarnaud@gmail.com>
2024-12-11 12:31:36 +0100

    feat(day11): solve part 2

--- aoc-2024/src/days/day11.rs
+++ aoc-2024/src/days/day11.rs
@@ -1,5 +1,5 @@
 use anyhow::{Context, Result};
-use std::fs;
+use std::{collections::HashMap, fs};
 
 fn split_even_digit(num: usize) -> (usize, usize) {
     let binding = num.to_string();
@@ -7,40 +7,53 @@ fn split_even_digit(num: usize) -> (usize, usize) {
     (nb1.parse().unwrap(), nb2.parse().unwrap())
 }
 
+fn insert_or_incr(map: &mut HashMap<usize, usize>, item: usize, count: usize) {
+    if map.contains_key(&item) {
+        let prev_count = map.get(&item).unwrap();
+        map.insert(item, count + prev_count);
+    } else {
+        map.insert(item, count);
+    }
+}
+
 pub fn run() -> Result<()> {
     let input = fs::read_to_string("inputs/day11.txt").context("Reading file")?;
 
-    let mut numbers: Vec<usize> = input
+    let numbers: Vec<usize> = input
         .split_whitespace()
         .map(|nb| nb.parse().unwrap())
         .collect();
 
+    let mut map: HashMap<usize, usize> = numbers.iter().map(|&nb| (nb, 1)).collect();
     let mut part1 = 0;
 
     for j in 0..75 {
-        let mut i = 0;
-        let mut length = numbers.len();
+        let mut temp_map: HashMap<usize, usize> = HashMap::new();
 
-        while i < length {
-            if numbers[i] == 0 {
-                numbers[i] = 1;
-            } else if numbers[i].to_string().len() % 2 == 0 {
-                let (n1, n2) = split_even_digit(numbers[i]);
-                numbers[i] = n1;
-                numbers.insert(i + 1, n2);
-                i += 1;
-                length += 1;
+        for (nb, i) in &map {
+            if *nb == 0 {
+                insert_or_incr(&mut temp_map, 1, *i);
+            } else if nb.to_string().len() % 2 == 0 {
+                let (n1, n2) = split_even_digit(*nb);
+                insert_or_incr(&mut temp_map, n1, *i);
+                insert_or_incr(&mut temp_map, n2, *i);
             } else {
-                numbers[i] *= 2024;
+                insert_or_incr(&mut temp_map, nb * 2024, *i);
             }
-            i += 1;
         }
+        map = temp_map;
         if j == 24 {
-            part1 = numbers.len();
+            for count in map.values() {
+                part1 += count;
+            }
         }
     }
 
-    println!("{:?}", part1);
-    println!("{:?}", numbers.len());
+    let mut part2 = 0;
+    for (_, count) in map {
+        part2 += count;
+    }
+    println!("Part 1: {:?}", part1);
+    println!("Part 2: {:?}", part2);
     Ok(())
 }
